b8b7ae4e79b8ce32f79744e04210c95a2f210c96
gabriellegabi329-lab <gabriellegabi329@gmail.com>
2025-08-15 12:25:11 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main() {
   // Definição das variáveis para a carta 1
-  char estado1 = A01, cidade1 = 1;
+  char estado1 = 'A';
+  int cidade1 = 1;
   int populacao1 = 132250, pontos_turisticos1 = 10;
   float area1 = 1200.1, pib1 = 45.75;
 
   // Definição das variáveis para a carta 2
-  char estado2 = B02, cidade2 = 2;
+  char estado2 = 'B';
+  int cidade2 = 2;
   int populacao2 = 567890, pontos_turisticos2 = 18;
   float area2 = 1200.5, pib2 = 22.32;
 
@@ -15,7 +17,7 @@ int main() {
   printf("Estado (A-H): ");
   scanf(" %c", &estado1);
   printf("Cidade (1-4): ");
-  scanf(" %c", &cidade1);
+  scanf("%d", &cidade1);
   printf("População: ");
   scanf("%d", &populacao1);
   printf("Área (km²): ");
@@ -30,7 +32,7 @@ int main() {
   printf("Estado (A-H): ");
   scanf(" %c", &estado2);
   printf("Cidade (1-4): ");
-  scanf(" %c", &cidade2);
+  scanf("%d", &cidade2);
   printf("População: ");
   scanf("%d", &populacao2);
   printf("Área (km²): ");
@@ -42,7 +44,7 @@ int main() {
 
   // Exibição dos dados da carta 1
   printf("\nDados da Carta 1:\n");
-  printf("Código: %c%c1\n", estado1, cidade1);
+  printf("Código: %c%02d\n", estado1, cidade1);
   printf("População: %d\n", populacao1);
   printf("Área: %.2f km²\n", area1);
   printf("PIB: %.2f milhões\n", pib1);
@@ -50,7 +52,7 @@ int main() {
 
   // Exibição dos dados da carta 2
   printf("\nDados da Carta 2:\n");
-  printf("Código: %c%c1\n", estado2, cidade2);
+  printf("Código: %c%02d\n", estado2, cidade2);
   printf("População: %d\n", populacao2);
   printf("Área: %.2f km²\n", area2);
   printf("PIB: %.2f milhões\n", pib2);
